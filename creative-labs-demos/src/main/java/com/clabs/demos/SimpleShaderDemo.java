e9777470669daec4eea0ac167ca0b92b23c9dca1
rdhouse <rutger.dijkhuizen@gmail.com>
2018-09-28 22:47:13 +0200

    added simple shaders files

---
+++ creative-labs-demos/src/main/java/com/clabs/demos/SimpleShaderDemo.java
@@ -0,0 +1,27 @@
+package com.clabs.demos;
+
+import com.clabs.engine.api.Game;
+import com.clabs.engine.core.EngineException;
+
+public class SimpleShaderDemo implements Game {
+
+    @Override
+    public void init() throws EngineException {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void destroy() throws EngineException {
+
+    }
+}
---
+++ creative-labs-demos/src/main/resources/com/clabs/demos/SimpleShaderDemo.fs
@@ -0,0 +1,9 @@
+#version 330
+
+in vec3 outColor;
+out vec4 fragColor;
+
+void main()
+g{
+    fragColor = vec4(outColor, 1.0);
+}

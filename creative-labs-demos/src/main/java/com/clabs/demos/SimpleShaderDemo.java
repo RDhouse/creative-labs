b718804068feb6f88a53bc43ecd1ee3aa3b31ac0
rdhouse <rutger.dijkhuizen@gmail.com>
2018-09-28 23:08:25 +0200

    implemented glsl shader

--- creative-labs-demos/src/main/java/com/clabs/demos/SimpleShaderDemo.java
+++ creative-labs-demos/src/main/java/com/clabs/demos/SimpleShaderDemo.java
@@ -2,12 +2,28 @@ package com.clabs.demos;
 
 import com.clabs.engine.api.Game;
 import com.clabs.engine.core.EngineException;
+import com.clabs.engine.shader.ShaderProgram;
+import com.clabs.engine.util.FileUtils;
 
 public class SimpleShaderDemo implements Game {
 
+    private ShaderProgram shaderProgram;
+
     @Override
     public void init() throws EngineException {
+        // create shader program.
+        shaderProgram = new ShaderProgram();
+        shaderProgram.createVertexShader(FileUtils.loadResource(this.getClass().getResource("simple_shader_demo_vertex.glsl").toString()));
+        shaderProgram.createFragmentShader(FileUtils.loadResource(this.getClass().getResource("simple_shader_demo_fragment.glsl").toString()));
+        shaderProgram.link();
 
+        // create vertices
+        float[] vertices = new float[] {
+                -0.5f, 0.5f, 0.5f,
+                -0.5f, -0.5f, 0.5f,
+                0.5f, -0.5f, 0.5f,
+                0.5f, 0.5f, 0.5f
+        };
     }
 
     @Override
@@ -17,11 +33,15 @@ public class SimpleShaderDemo implements Game {
 
     @Override
     public void render() {
-
+        shaderProgram.bind();
+        // TODO:
+        shaderProgram.unbind();
     }
 
     @Override
     public void destroy() throws EngineException {
-
+        if (shaderProgram != null) {
+            shaderProgram.destroy();
+        }
     }
 }
